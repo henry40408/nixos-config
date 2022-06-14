727cebafaee77ff463d8369e69ecbd49cd29bacc
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-06-14 23:19:54 +0800

    feat: set hardware clock in local time

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,7 @@
 
   # Set your time zone.
   time.timeZone = "Asia/Taipei";
+  time.hardwareClockInLocalTime = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
