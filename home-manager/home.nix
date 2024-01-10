87594d7aaa70b47ef588af03b1a7dccd14c15a5b
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-01-10 15:08:18 +0000

    feat: zsh plugins

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -45,7 +45,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  home.packages = with pkgs; [ fd gnumake ];
+  home.packages = with pkgs; [ fd gnumake gping procs ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
@@ -62,6 +62,8 @@
   };
 
   programs.atuin.enable = true;
+  programs.bat.enable = true;
+  programs.fzf.enable = true;
   programs.gh = {
     enable = true;
     settings = {
@@ -70,7 +72,12 @@
   };
   programs.git = {
     enable = true;
-    delta = { enable = true; };
+    delta = {
+      enable = true;
+      options = {
+        side-by-side = true;
+      };
+    };
     signing = {
       key = "2316687+henry40408@users.noreply.github.com";
       signByDefault = true;
@@ -79,6 +86,7 @@
     userEmail = "2316687+henry40408@users.noreply.github.com";
   };
   programs.gpg.enable = true;
+  programs.lsd.enable = true;
   programs.neovim.enable = true;
   programs.ripgrep.enable = true;
   programs.tmux = {
@@ -120,13 +128,41 @@
   };
   programs.zsh = {
     enable = true;
+    enableAutosuggestions = true;
     initExtra = (builtins.readFile ./zshrc.extra.zsh);
     initExtraFirst = (builtins.readFile ./zshrc.extraFirst.zsh);
     oh-my-zsh = {
       enable = true;
-      plugins = ["common-aliases" "git" "gpg-agent"];
+      plugins = [
+        "aws"
+        "command-not-found"
+        "common-aliases"
+        "direnv"
+        "fzf"
+        "gem"
+        "git"
+        "git-extras"
+        "git-flow"
+        "gitignore"
+        "golang"
+        "gpg-agent"
+        "helm"
+        "pip"
+        "python"
+        "rails"
+        "ruby"
+      ];
     };
     plugins = [
+      {
+        name = "fzf-tab";
+        src = pkgs.fetchFromGitHub {
+          owner = "Aloxaf";
+          repo = "fzf-tab";
+          rev = "e85f76a";
+          sha256 = "sha256-Qf9AZ2DjO9QUyEF7QG8JtlBHjwHfINOJkrMfu7pipns=";
+        };
+      }
       {
         name = "powerlevel10k";
         src = pkgs.fetchFromGitHub {
@@ -135,7 +171,34 @@
           rev = "v1.18.0";
           sha256 = "sha256-IiMYGefF+p4bUueO/9/mJ4mHMyJYiq+67GgNdGJ6Eew=";
         };
-	file = "powerlevel10k.zsh-theme";
+        file = "powerlevel10k.zsh-theme";
+      }
+      {
+        name = "you-should-use";
+        src = pkgs.fetchFromGitHub {
+          owner = "MichaelAquilina";
+          repo = "zsh-you-should-use";
+          rev = "773ae5f";
+          sha256 = "sha256-g4Fw0TwyajZnWQ8fvJvobyt98nRgg08uxK6yNEABo8Y=";
+        };
+      }
+      {
+        name = "zsh-autopair";
+        src = pkgs.fetchFromGitHub {
+          owner = "hlissner";
+          repo = "zsh-autopair";
+          rev = "9d003fc";
+          sha256 = "sha256-hwZDbVo50kObLQxCa/wOZImjlH4ZaUI5W5eWs/2RnWg=";
+        };
+      }
+      {
+        name = "zsh-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-completions";
+          rev = "20f3cd5";
+          sha256 = "sha256-f+FXQIks4nUFabL04fgdgwOI6WTPq6mqZ/+Jvne2CRM=";
+        };
       }
       {
         name = "zsh-secrets";
@@ -147,8 +210,15 @@
         };
       }
     ];
+    shellAliases = {
+      cat = "bat";
+      ls = "lsd";
+      ping = "gping";
+      ps = "procs";
+    };
     syntaxHighlighting = { enable = true; };
   };
+  programs.zoxide.enable = true;
 
   services.gpg-agent = {
     enable = true;
