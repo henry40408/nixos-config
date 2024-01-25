e81b46a4596479b9d3eee7484bc12caafbe14f85
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-01-25 21:52:08 +0800

    refactor: merge neovim lua config

--- home-manager/common/neovim/lua/plugins/lsp.lua
+++ home-manager/common/neovim/lua/plugins/lsp.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, {
+          "nix",
+        })
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
--- home-manager/common/neovim/lua/plugins/mason.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- ref: https://github.com/williamboman/mason.nvim/issues/428#issuecomment-1357203892
-  { "williamboman/mason.nvim", opts = { PATH = "append" } },
-}
