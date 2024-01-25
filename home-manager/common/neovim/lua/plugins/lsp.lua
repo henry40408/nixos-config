cb223dc28807cfe0331c98dcae1673edb20981a5
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-01-25 22:55:54 +0800

    feat: add csv treesitter to neovim

--- home-manager/common/neovim/lua/plugins/lsp.lua
+++ home-manager/common/neovim/lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, {
+          "csv",
           "nix",
         })
       end
