71f4a464edac8caccc1ffaceb3c4fdc910040793
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-05-20 10:14:20 +0800

    chore: upgrade lazyvim to 11.3.2

--- home-manager/common/neovim/lua/config/lazy.lua
+++ home-manager/common/neovim/lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", version = "v10.22.0" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", version = "v11.3.2" },
     { import = "lazyvim.plugins.extras.editor.navic" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
